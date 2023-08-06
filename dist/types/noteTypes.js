7f29f72580f71acc32e1b1d1d301369ac1df1c12
Deny-ux <denys.fokashchuk@gmail.com>
2023-08-06 12:33:46 +0200

    Removed dist folder

--- dist/types/noteTypes.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categories = void 0;
-exports.categories = ["Task", "Idea", "Random Thought"];
--- dist/validators/validCategory.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidCategory = void 0;
-const noteTypes_1 = require("../types/noteTypes");
-const isValidCategory = (x) => noteTypes_1.categories.includes(x);
-exports.isValidCategory = isValidCategory;
